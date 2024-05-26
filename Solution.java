6d8882f07649fcb84c8ee00dde89e1a0480ce093
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-26 12:07:57 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+
+    private static final int[] RANGE_OF_VALUES = {1, 50};
+
+    public int duplicateNumbersXOR(int[] input) {
+        int[] frequency = new int[RANGE_OF_VALUES[1] + 1];
+        int xorDuplicates = 0;
+        for (int value : input) {
+            if (++frequency[value] == 2) {
+                xorDuplicates ^= value;
+            }
+        }
+        return xorDuplicates;
+    }
+}
